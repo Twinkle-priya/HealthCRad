2dad5326d6221c51b394d6550bc1dbfa533227a8
Twinkle priya <102142631+Twinkle-priya@users.noreply.github.com>
2024-04-09 15:23:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans"],
+      kanit: ["Kanit", "sans"],
+      inter: ["Inter", "sans"],
+    },
+    // colors: {
+    //   "#1A30A1": "#1A30A1",
+    // },
+  },
+  plugins: [],
+};
